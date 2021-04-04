cec6af78e1fd09354e7aa480de9cd884cef5adca
azerpas <xskylix@hotmail.fr>
2021-04-04 16:50:18 +0200

    ex10

---
+++ TD4/ex10.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+int value = 0;
+void *runner(void *param); /* the thread */
+int main(int argc, char * argv[])
+{
+    pthread_t tid;
+    pthread_attr_t attr;
+    pid_t pid;
+    void *status;
+    pid = fork();
+    if (pid == 0) {
+        pthread_attr_init(&attr);
+        pthread_create(&tid,&attr,runner,NULL);
+        pthread_join(tid,&status);
+        pthread_exit(status);
+    }
+    else if (pid > 0) { 
+        int v = -1;
+        wait(&v);
+        printf("Value is: %d", v); /* LINE P */
+    }
+}
+void *runner(void *param) {
+    char input[10];
+    printf("Please enter a value: ");
+    fgets(input, 10, stdin);
+    value = atoi(input);
+    int sum = 0;
+    for (size_t i = 0; i <= value; i++)
+    {
+        sum += i;
+    }
+    printf("Sum is %d", sum);
+    return sum;
+}
