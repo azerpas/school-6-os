2bb18065c859802dc1af70d79e694fe18d29970a
azerpas <xskylix@hotmail.fr>
2021-05-13 22:37:50 +0200

    Finished TP Part2 - 1

--- TP1/part2/1.c
+++ TP1/part2/1.c
@@ -1,6 +1,10 @@
-// C program to illustrate
-// pipe system call in C
-// shared by Parent and Child
+/**
+ * @author @azerpas
+ * Write Linux C program to create two processes: P1 (the parent process) and P2 (the child process
+of P1). The parent process P1 takes a string and passes it to the child process P2. P2 concatenates the
+received string with another string without using string function and sends it back to P1 for printing. You
+must use pipe() in your solution
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,12 +15,15 @@ char* msg1 = "hello";
 char* concat(char* a, char* b){
     size_t l1 = 0;
     size_t l2 = 0;
+    // Get length of a
     while(a[l1] != '\0'){
         ++l1;
     }
+    // Get length of b
     while(b[l2] != '\0'){
         ++l2;
     }
+    // Create 'c' string
     char c[l1+l2+1];
     for (size_t i = 0; i < l1; i++){
         c[i] = a[i];
@@ -24,7 +31,6 @@ char* concat(char* a, char* b){
     for (size_t i = 0; i < l2; i++){
         c[i+l2] = b[i];
     }
-    printf("After second loop: %s\n",c);
     c[l1+l2] = '\0';
     return c;
     
@@ -32,30 +38,40 @@ char* concat(char* a, char* b){
 
 int main()
 {
-    char inbuf[MSGSIZE];
+    char inbuf[MSGSIZE], final[MSGSIZE];
     int p[2], t[2], nbytes;
     pid_t pid;
+    // Create pipes
     pipe(p);
     pipe(t);
     pid = fork();
-    /* continued */
     if (pid == 0) {
         // SON
+        // Closing writing pipe of 'p'
         close(p[1]);
+        // Reading from pipe 'p' (father message)
         while ((nbytes = read(p[0], inbuf, MSGSIZE)) > 0)
             printf("%s \n", inbuf);
         if (nbytes != 0)
             exit(2);
         printf("Concat \n");
+        // Concatenate
         char* concatened = concat(inbuf, "world");
         printf("Finished concat '%s' \n", concatened);
+        // Send message to father with pipe 't'
+        write(t[1], concatened, MSGSIZE);
+        close(t[1]);
         return 0;
     }
     else {
         // FATHER
         close(t[1]);
+        // Sending message to son with pipe 'p'
         write(p[1], msg1, MSGSIZE);
         close(p[1]);
+        // Reading back the message from pipe 't'
+        while ((nbytes = read(t[0], final, MSGSIZE)) > 0)
+            printf("Feedback from son: '%s' \n", final);
         wait(NULL);
     }
 }
