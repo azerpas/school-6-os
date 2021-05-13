b461314e2750a2037b81999e495ff8599724d959
azerpas <xskylix@hotmail.fr>
2021-05-13 22:32:15 +0200

    Updated TP1 Part 2 - 1

--- TP1/part2/1.c
+++ TP1/part2/1.c
@@ -8,29 +8,38 @@
 #define MSGSIZE 80
 char* msg1 = "hello";
 
-void concat(char* a, char* b){
-    int len = sizeof(a)-1;
-    int i = 0;
-    printf("%c",a[2]);
-    while(b[i] != '\0'){
-        printf("b: %c\n", b[i]);
-        printf("a[%d]: ",len);
-        printf("%c\n", a[len]);
-        a[len] = b[i];
-        printf("%s \n", a);
-        i++;
-        len++;
+char* concat(char* a, char* b){
+    size_t l1 = 0;
+    size_t l2 = 0;
+    while(a[l1] != '\0'){
+        ++l1;
     }
+    while(b[l2] != '\0'){
+        ++l2;
+    }
+    char c[l1+l2+1];
+    for (size_t i = 0; i < l1; i++){
+        c[i] = a[i];
+    }
+    for (size_t i = 0; i < l2; i++){
+        c[i+l2] = b[i];
+    }
+    printf("After second loop: %s\n",c);
+    c[l1+l2] = '\0';
+    return c;
+    
 }
 
 int main()
 {
     char inbuf[MSGSIZE];
-    int p[2], nbytes;
+    int p[2], t[2], nbytes;
     pid_t pid;
     pipe(p);
+    pipe(t);
+    pid = fork();
     /* continued */
-    if (pid = fork() == 0) {
+    if (pid == 0) {
         // SON
         close(p[1]);
         while ((nbytes = read(p[0], inbuf, MSGSIZE)) > 0)
@@ -38,12 +47,13 @@ int main()
         if (nbytes != 0)
             exit(2);
         printf("Concat \n");
-        concat(inbuf, " world");
-        printf("Finished concat '%s' \n", inbuf);
+        char* concatened = concat(inbuf, "world");
+        printf("Finished concat '%s' \n", concatened);
         return 0;
     }
     else {
         // FATHER
+        close(t[1]);
         write(p[1], msg1, MSGSIZE);
         close(p[1]);
         wait(NULL);
